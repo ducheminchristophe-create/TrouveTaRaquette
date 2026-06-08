5974b4b3aac04b9a18414b1fa2d9667f444ea708
ducheminchristophe-create <duchemin.christophe@gmail.com>
2026-06-08 14:47:02 +0200

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  // Permet d'importer les fichiers JSON depuis src/data
-  // Pas besoin de config spéciale — Next.js supporte les imports JSON nativement
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
